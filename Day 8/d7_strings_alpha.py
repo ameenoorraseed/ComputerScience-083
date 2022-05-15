f42194edea2d7db131e981745ad4057542807186
Ameenoor Raseed A R <42912863+ameenoorraseed@users.noreply.github.com>
2022-05-15 22:03:43 +0530

    Delete Day 8 directory

--- Day 8/d7_strings_alpha.py
+++
@@ -1,9 +0,0 @@
-
-
-a = '123'
-b = 'kiwi'
-c = 'password123'
-
-print( a.isalpha() ) # checks for only alphabet 
-print( b.isalpha() )
-print( c.isalpha() )
--- Day 8/d7_strings_find.py
+++
@@ -1,5 +0,0 @@
-a = 's123password'
-	#012345678910
-
-print( a.find('d') ) # returns the index of inserted char
-print( a.find('z') ) # returns -1 if not found
