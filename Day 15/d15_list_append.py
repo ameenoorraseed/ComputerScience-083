8f0a0ddc4de7cb4f4dcb6c813bc3c9157e7a044f
Ameenoor Raseed A R <42912863+ameenoorraseed@users.noreply.github.com>
2023-05-19 18:00:17 +0530

    Add files via upload

---
+++ Day 15/d15_list_append.py
@@ -0,0 +1,8 @@
+# how to add elements into the list
+
+my_list = [ ]
+print(my_list)
+
+my_list.append(55)
+my_list.append(100)
+print(my_list)
---
+++ Day 15/d15_list_del.py
@@ -0,0 +1,7 @@
+# pop() - removes the last element from the list
+# delete - del is a keyword which is used to delete elements in a list
+
+my_list = [44,1,-4,8,4,32,0.3]
+del my_list[0:5]
+
+print(my_list)
